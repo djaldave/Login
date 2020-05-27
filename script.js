f8802fa0bfbc8c2ef16324d732f9281ca15a18ce
slasherdavidkun <slasherdavidkun@gmail.com>
2020-05-27 19:09:22 +0800

    it works

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
 if(document.getElementsByClassName('firstForm')[0]){
 	document.getElementsByClassName('firstForm')[0].onsubmit = function(e){
+		document.getElementById("pop").style.display = "none";
 		e.preventDefault();
 		const field = this;
-		let username = field.querySelector('input[type=text]');
+		let username = field.querySelector('input[type=email]');
 		let pass = field.querySelector('input[type=password]');
 
-		if((username.value === 'admin' && pass.value === 'admin') ||
-			((username.value === 'user' && pass.value === 'user') )){
-			alert("It works")
+		if((username.value === 'admin' && pass.value === 'admin') ){
+			allFunc('pop');
+		}else if ((username.value === 'user' && pass.value === 'user') ){
+			allFunc('pop1');
 		} else {
-			alert('Credentials are invalid')
+			alert('Credentials are invalid');
 		}
 	}
+}
+function showMsg(varName)
+{
+	$("#"+varName).fadeIn('slow', function () {
+		$(this).delay(3000).fadeOut('slow');
+	});
+}
+function clearInputs() {
+	document.getElementById("username").value = '';
+	document.getElementById("password").value = '';
+}
+function addClass(varName) {
+	let v = document.getElementById(varName);
+	v.classList.add('alert', 'alert-success', 'alert-dismissible', 'fade', 'show', 'alertMsg');
+
+
+}
+function removeClass(varName) {
+	var element = document.getElementById(varName);
+	element.classList.remove("alert alert-success alert-dismissible fade show alertMsg");
+}
+function allFunc(varName) {
+	addClass(varName);
+	showMsg(varName);
+	// removeClass();
+	clearInputs();
+
 }
